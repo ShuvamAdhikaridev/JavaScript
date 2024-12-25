6f5d74a7e48788419554b476e0d6c0f0c0f06f02
ShuvamAdhikaridev <shuvam.adhikar.dev@gmail.com>
2024-12-25 23:10:01 +0545

    Conversion

---
+++ conversionOperations.js
@@ -0,0 +1,19 @@
+// let score = false;
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+// let valueInNumer = Number(score);
+// console.log(typeof valueInNumer);
+// console.log(valueInNumer);
+
+// let isLoggedIn = "Shvuam";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
