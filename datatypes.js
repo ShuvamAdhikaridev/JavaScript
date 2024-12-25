9d7ce812d180afc124db4be6ecf84119eec8f44d
ShuvamAdhikaridev <shuvam.adhikar.dev@gmail.com>
2024-12-25 19:03:11 +0545

    Data-types

---
+++ datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all JS code as newer version
+
+// alert[3 + 3]; we are using nodejs not browser
+
+let name = "Shuvam";
+let age = 21;
+let isLoggedIn = true;
+
+//number ==> 2 to power 53
+// bigint
+//string ==> ""
+
+//boolean ==> true/false
+//null ==> standalone value
+// undefined ==>
+//symbol ==> unique
+
+//object
+
+console.log(typeof null);
---
+++ variables.js
@@ -0,0 +1,15 @@
+const accountId = 1223344;
+
+let accountEmail = "shuvamlim234@gmail.com";
+
+var accountPassword = "12345";
+
+accountCity = "Kathmandu";
+
+//accountId = 2;
+accountEmail = "node@gm.com";
+accountPassword = "1122222";
+accountCity = "Chabahil";
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accountCity, accountPassword]);
