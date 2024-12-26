17696e931bad95e4db831540bf829a19ee560036
ShuvamAdhikaridev <shuvam.adhikar.dev@gmail.com>
2024-12-27 00:18:51 +0545

    Day-3

--- dataTypes-summary.js
+++ dataTypes-summary.js
@@ -2,32 +2,32 @@
 
 // 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
-const score = 100;
-const scoreValue = 100.3;
+// const score = 100;
+// const scoreValue = 100.3;
 
-const isLoggedIn = false;
-const outsideTemprature = null;
-let userEmail;
+// const isLoggedIn = false;
+// const outsideTemprature = null;
+// let userEmail;
 
-const id = Symbol(`123`);
-const anotherId = Symbol(`123`);
+// const id = Symbol(`123`);
+// const anotherId = Symbol(`123`);
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-const bigNumber = 345454348438383345454n;
+// const bigNumber = 345454348438383345454n;
 
 //   Reference [Non Primitive]
 
 // Array, Objects, Functions
 
-const heros = ["Spiderman", "Batman"];
-let myObj = {
-  name: "Shvuam",
-  age: 11,
-};
+// const heros = ["Spiderman", "Batman"];
+// let myObj = {
+//   name: "Shvuam",
+//   age: 11,
+// };
 
-const myFunction = function () {
-  console.log("Hello World");
-};
+// const myFunction = function () {
+//   console.log("Hello World");
+// };
 
-console.log(typeof outsideTemprature);
+// console.log(typeof outsideTemprature);
