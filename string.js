ad11f58d624ec745db36fc8a191367438660f822
ShuvamAdhikaridev <shuvam.adhikar.dev@gmail.com>
2024-12-31 00:06:34 +0545

    math_num

--- string.js
+++ string.js
@@ -3,22 +3,35 @@ const repoCount = 50;
 
 // console.log(name + repoCount + " Value");
 
-console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+// console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
 
-const gameName = new String(`Shuvam-Ad`);
+const gameName = new String(`Shuvam-Ad-com-Hello-Ok`);
 
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
 
-console.log(gameName.charAt(4));
+// console.log(gameName.charAt(4));
 
-console.log(gameName.indexOf(`a`));
+// console.log(gameName.indexOf(`a`));
 
-const newString = gameName.substring(0, 5);
+const newString = gameName.substring(0, 4);
 console.log(newString);
 
-const anotherString = gameName.slice(-10, 3);
+const anotherString = gameName.slice(0, 3);
 console.log(anotherString);
+
+const newStringOne = "  Shuvam   ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "Https://Escobar.com/Escobar%7okok";
+
+console.log(url.replace(`%7`, `-`));
+
+console.log(url.includes(`okok`));
+console.log(url.includes(`hello`));
+
+console.log(gameName.split(`-`));
