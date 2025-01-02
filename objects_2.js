ac16767152e00be8ed3292ae5f5dba258f94051c
ShuvamAdhikaridev <shuvam.adhikar.dev@gmail.com>
2025-01-02 19:57:36 +0545

    moreObjects

---
+++ objects_2.js
@@ -0,0 +1,44 @@
+const tinderUser = new Object(); // Singleton
+
+tinderUser.id = "123abc";
+tinderUser.name = "Shuvam";
+tinderUser.loggedIn = false;
+
+// console.log(tinderUser);
+
+const regularUser = {
+  email: "Yellow@gmail.com",
+  fullName: {
+    firstname: "shuvam",
+    lastname: "Adhikari",
+  },
+};
+
+// console.log(regularUser.fullName.firstname);
+
+const obj1 = { 1: "a", 2: "b" };
+const obj2 = { 3: "a", 4: "b" };
+
+// const obj3 = Object.assign({}, obj1, obj2);
+// console.log(obj3);
+
+const obj3 = { ...obj1, ...obj2 };
+
+// console.log(obj1 === obj3);
+console.log(obj3);
+
+const users = [
+  {
+    id: 1,
+    email: "User@gmail.com",
+  },
+];
+
+console.log(tinderUser);
+
+console.log(Object.keys(tinderUser));
+
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty(`name`));
