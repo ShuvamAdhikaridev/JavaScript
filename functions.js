d2034ef41b61e63f0d886e6d7805696f9ab38250
ShuvamAdhikaridev <shuvam.adhikar.dev@gmail.com>
2025-01-03 23:16:46 +0545

    Day2_functions

---
+++ functions.js
@@ -0,0 +1,38 @@
+function sayMyName() {
+  console.log("S");
+  console.log("H");
+  console.log("U");
+  console.log("V");
+  console.log("A");
+  console.log("M");
+}
+
+// sayMyName();
+
+// function addTwoNums(num1, num2) {
+//   // paramteres
+//   console.log(num1 + num2);
+// }
+function addTwoNums(num1, num2) {
+  // paramteres
+  //   let result = num1 + num2;
+  //   console.log("shuvam");
+  //   return result;
+  return num1 + num2;
+}
+const result = addTwoNums(2, 5); // Argument
+
+// console.log(result);
+
+function loginUserMessage(username) {
+  if (!username) {
+    console.log(`Please enter a username`);
+    return;
+  }
+  return `${username} just logged in.`;
+}
+
+// console.log(loginUserMessage("Shuvam"));
+// console.log(loginUserMessage());
+
+f;
