a258eeb429fd4d0bed1b2006309eed8807787427
ShuvamAdhikaridev <shuvam.adhikar.dev@gmail.com>
2025-01-06 18:57:32 +0545

    Arrow Function

---
+++ arrow.js
@@ -0,0 +1,43 @@
+const user = {
+  username: "Shuvam",
+  price: 999,
+
+  welcomeMessage: function () {
+    console.log(`${this.username}, Welcome to the Website`);
+    console.log(this);
+  },
+};
+
+// user.welcomeMessage();
+// user.username = "Sam";
+// user.welcomeMessage();
+
+// console.log(this);
+
+// function one() {
+//   let username = "shuvam";
+//   console.log(this.username);
+// }
+
+// one();
+
+const one = () => {
+  let username = "Shuvam";
+  console.log(this);
+};
+// one();
+
+// const addTwo = (num1, num2) => {
+//   return num1 + num2;
+// };
+
+// const addTwo = (num1, num2) => num1 + num2; /* implicit return */
+
+// const addTwo = (num1, num2) => {
+//   num1 + num2;
+// };
+
+const addTwo = (num1, num2) => ({
+  username: "shuvam",
+});
+console.log(addTwo(3, 3));
