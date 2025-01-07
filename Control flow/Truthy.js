b9493862e1b836c0d25e08de1e6d84ff0c050f22
ShuvamAdhikaridev <shuvam.adhikar.dev@gmail.com>
2025-01-07 22:55:49 +0545

    Checking whether the object/array is empty or not using length

---
+++ Control flow/Truthy.js
@@ -0,0 +1,17 @@
+const userEmail = [];
+
+// if (userEmail) {
+//   console.log(userEmail);
+// } else {
+//   console.log(`No User email`);
+// }
+
+// if (userEmail.length === 0) {
+//   console.log("Array is empty");
+// }
+
+const emptyobj = {};
+
+if (Object.keys(emptyobj).length === 0) {
+  console.log(`Object is empty`);
+}
