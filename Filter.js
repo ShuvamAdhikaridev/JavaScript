47ccb3397a89e80afaf829028150e1acc7c14fa3
ShuvamAdhikaridev <shuvam.adhikar.dev@gmail.com>
2025-01-09 18:31:32 +0545

    Filter

---
+++ Filter.js
@@ -0,0 +1,24 @@
+// const coding = ["JS", "Ruby", "Java", "Python", "Cpp"];
+
+// const values = coding.forEach((item) => {
+//   console.log(item);
+//   return item;
+// });
+
+// console.log(values);
+
+const mNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const newNums = mNums.filter((num) => {
+  num > 5;
+});
+
+// const newNums = [];
+
+// mNums.forEach((num) => {
+//   if (num > 4) {
+//     newNums.push(num);
+//   }
+// });
+
+console.log(newNums);
